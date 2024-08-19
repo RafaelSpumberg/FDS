c438e318110b14a90589414156453341a9923737
RafaelSpumberg <150760513+RafaelSpumberg@users.noreply.github.com>
2024-08-19 23:22:26 +0000

    SUVFlex

--- ExercicioMotivadorDoUsoDePadroes-main/ExercicioMotivadorDoUsoDePadroes-main/src/App.java
+++ ExercicioMotivadorDoUsoDePadroes-main/ExercicioMotivadorDoUsoDePadroes-main/src/App.java
@@ -23,6 +23,21 @@ public class App {
         esportivo.viaja(30);
 
         System.out.println(esportivo);
-        System.out.println(esportivo);
+        /*
+         * Crie um novo modelo de carro chamado "SUVFlex". Este é FLEX (gasolina ou
+   alcool) e consome 8 Km/lt de gasolina ou 6 Km/lt de alcool. O tanque é "FLEX"
+   e tem 65 litros de capacidade. Por simplicidade assuma que o conteúdo do tanque 
+   é sempre o do último abastecimento.
+         */
+
+        Carro SUVFlex = new Carro("SUVFlex",TipoCombustivel.GASOLINA,6,45);
+
+        System.out.println("Tipo: ");
+        System.out.println(SUVFlex);
+        SUVFlex.abastece(TipoCombustivel.GASOLINA, 10);
+        SUVFlex.viaja(30);
+
+        System.out.println(SUVFlex);
+        
     }
 }
